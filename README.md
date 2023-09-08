9281eb7c5d29736ecc7623b341765b9c01e35190
gracecatk <gracie.kiker@gmail.com>
2023-09-08 17:20:30 -0500

    added ss to readme

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Password Generator Starter Code
+![image](https://github.com/gracecatk/challende-03/assets/140928469/cd174aa3-511b-424d-af99-41b93bb80e1c)
--- script.js
+++ script.js
@@ -1,19 +1,50 @@
+// Character sets or options for user
+const lowerCase = 'abcdefghijklmnopqrstuvwxyz';
+const upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const numbers = '1234567890';
+const specialChar = '!@#$%^&*()_+<>?';
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-// made the button clickable here to the log
+// Function to prompt the user for password criteria and generate a password
 function generatePassword() {
-  console.log('you clicked me?! how dare you.')
-
-// 1 Need to prompt the user for password length, upper, lower, numbers & special characters
-//    length between 8 < 128
-//    lower, upper, numbers, special char.
-//2 validate input
-//3 display the generated password
-
-
-
-  return "Your Custom Password"; //made this display where the password will go
+  // Prompt the user for password length
+  var passwordLength = prompt("Enter the length of the password (between 8 and 128 characters):");
+  
+  // Validate input for password length
+  if (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
+    alert("Invalid password length. Please enter a number between 8 and 128.");
+    return ""; // Return an empty string if the input is invalid.
+  }
+  
+  // Confirm which character sets to include
+  var useLowerCase = confirm("Include lowercase letters?");
+  var useUpperCase = confirm("Include uppercase letters?");
+  var useNumbers = confirm("Include numbers?");
+  var useSpecialChar = confirm("Include special characters?");
+  
+  // Validate that at least one character set is selected
+  if (!useLowerCase && !useUpperCase && !useNumbers && !useSpecialChar) {
+    alert("At least one character set must be selected.");
+    return ""; // Return an empty string if no character set is selected.
+  }
+  
+  // Create a character set based on user selections
+  var charset = "";
+  if (useLowerCase) charset += lowerCase;
+  if (useUpperCase) charset += upperCase;
+  if (useNumbers) charset += numbers;
+  if (useSpecialChar) charset += specialChar;
+  
+  // Generate the password
+  var password = "";
+  for (var i = 0; i < passwordLength; i++) {
+    var randomIndex = Math.floor(Math.random() * charset.length);
+    password += charset.charAt(randomIndex);
+  }
+  
+  return password;
 }
 
 // Write password to the #password input
@@ -22,7 +53,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
